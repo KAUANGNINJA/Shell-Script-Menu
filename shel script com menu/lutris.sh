f71e83357d2ecfacfe58df4f305e53470ba59125
KAUAN COSTA <75103913+KAUANGNINJA@users.noreply.github.com>
2021-02-13 00:12:24 -0300

    Delete shel script com menu directory

--- shel script com menu/lutris.sh
+++
@@ -1,8 +0,0 @@
-sudo rm /var/lib/dpkg/lock-frontend ; 
-sudo rm /var/cache/apt/archives/lock ;
-sudo apt update ;
-sudo apt upgrade -y ;
-
-sudo add-apt-repository ppa: lutris-team / lutris ;
-sudo apt update ;
-sudo apt install lutris -y ;
