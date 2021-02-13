f8c2684e3f9bd37780eb3fe6978a79d5f9cc5212
KAUAN COSTA <75103913+KAUANGNINJA@users.noreply.github.com>
2021-02-13 00:25:46 -0300

    Delete cs_1.6.sh

--- cs_1.6.sh
+++
@@ -1,7 +0,0 @@
-sudo rm /var/lib/dpkg/lock-frontend ; 
-sudo rm /var/cache/apt/archives/lock ;
-sudo apt update ;
-sudo apt upgrade -y ;
-
-sudo wget http://files.gs2us.com/cs_16.exe ;
-sudo chmod 777 cs_16.exe ;
